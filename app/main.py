a6ee3d3c94634d7c25149e344ce191fb2f292d56
Orest <dk.horible@gmail.com>
2026-04-13 19:57:54 +0100

    fixx

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def create_person_list(people: list) -> list:
         if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
         if person.get("husband"):
-            Person.people[person["name"]].husband = (
-                Person.people)[person["husband"]]
+            Person.people[person["name"]].husband \
+                = Person.people[person["husband"]]
 
     return person_list
